e2209e8292e0f0067e82045f261bfc21c1c1c373
wangfeng1996 <1290715192@qq.com>
2019-11-12 13:58:29 +0800

    iterator接口

---
+++ JavaBasicOther/src/com/day02_code/demo02/test01.java
@@ -0,0 +1,14 @@
+package com.day02_code.demo02;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * 介绍iterator接口
+ *
+ */
+public class test01 {
+    public static void main(String[] args) {
+        Collection<String> iter = new ArrayList();
+    }
+}
