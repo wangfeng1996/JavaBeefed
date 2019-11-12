192c3f8b929b4faf5f0a432cd91be8456d1ddf28
wangfeng1996 <1290715192@qq.com>
2019-11-12 14:06:52 +0800

    iterator接口

--- JavaBasicOther/src/com/day02_code/demo02/test01.java
+++ JavaBasicOther/src/com/day02_code/demo02/test01.java
@@ -4,11 +4,18 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 /**
- * 介绍iterator接口
- *
+ * 介绍iterator接口----迭代器
  */
 public class test01 {
     public static void main(String[] args) {
         Collection<String> iter = new ArrayList();
+        //在集合中添加对象
+        iter.add("hello");
+        iter.add("world");
+        iter.add("java");
+        iter.add("C++");
+        iter.add("python");
+
     }
+
 }
